f3011a9fea0e8d16b7b9cbca788a3e9bf7fb83c0
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-02 16:57:49 +0100

    build.sh: cleanup

--- docker/build.sh
+++ docker/build.sh
@@ -11,49 +11,14 @@
 ## prepare SDK environment
 source ~/env.sh
 
-## TODO rm
-#export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
-#export ZEPHYR_SDK_INSTALL_DIR=$HOME/zephyr-sdk-0.10.3
+## build for reel board
+cd ~/zephyrproject/zephyr
 
-# ## set up 'unset preset flags' command script
-# echo 'unset CFLAGS CXXFLAGS' >> /etc/profile.d/unset_cflags.sh
-#
-# ## TODO shift this to docker file?
-# ## install zephyr sources
-# pip3 install west
-#
-# ## clone zephyr repository
-# west init zephyrproject
-# cd zephyrproject
-# west update
-#
-# ## install python dependencies
-# pip3 install -r zephyr/scripts/requirements.txt
-   
-## zephyr setup
-#cd ~/zephyrproject
-#west init --mr=v1.14.1 ~/zephyrproject
-#west update
-#pip3 install -r zephyr/scripts/requirements.txt
-
-## nordic PCA10040 v1.2.4 board
-## tool collection (segger, nrf...)
-# cd ~/zephyrproject && tar -xvJf /mnt/gcc-arm-none-eabi-8-2019-q3-update-linux-pt1.tar.xz
-# cd ~/zephyrproject && tar -xvJf /mnt/gcc-arm-none-eabi-8-2019-q3-update-linux-pt2.tar.xz
-# apt-get install -y build-essential flex bison libncurses5
-# dpkg -i /mnt/JLink_Linux_V644e_x86_64.deb
-# dpkg -i /mnt/nRF-Command-Line-Tools_10_3_0_Linux-amd64.deb
-
-# TODO rm   
-## build hello world
-#cd ~/zephyrproject/zephyr
-#source zephyr-env.sh
-#west build -b reel_board samples/hello_world
+## clean
+test -d build && rm -rf build
 
 ## build for nrf52_pca10040
-cd ~/zephyrproject
-source ~/env.sh
-west build -b nrf52_pca10040 zephyr/samples/bluetooth/beacon
+west build -b nrf52_pca10040 samples/bluetooth/beacon
 
 ## obtain build artifacts
 west flash --erase
